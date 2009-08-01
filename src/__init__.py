f498f16923a7248591ebc8bd464113d7ebbfbe84
Ram Rachum <cool-rr@cool-rr.com>
2009-08-01 14:14:05 +0300

    Did it as a package

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+"""
+An educational environment for learning Python, suitable for beginners
+and children. Inspired by LOGO.
+
+Call `run()` to run it.
+
+PythonTurtle aims to be the most low-threshold way for learning or
+teaching Python. The user is given command of an interactive Python
+shell, similar to IDLE, and is able to use Python functions to move a
+turtle that is displayed on the screen. An illustrated help screen
+demonstrates how to move the turtle and introduces the student to the
+basics of Python programming.
+"""
+
+from pythonturtle import run
+
+if __name__=="__main__":
+    run()
