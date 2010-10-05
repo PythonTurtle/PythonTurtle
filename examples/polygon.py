67e1335f0ae22729897f0fdf60638f07e50b94da
Spacerat <spacerat3004@gmail.com>
2010-10-05 22:49:14 +0100

    Added an example script.

---
+++ examples/polygon.py
@@ -0,0 +1,13 @@
+def Polygon(l,n):
+    #calculate the polygon's internal angles.
+    ang = 360/n
+    
+    #draw the polygon
+    for i in xrange(n):
+        go(l)
+        left(ang)
+		
+
+	
+
+Polygon(20,5)
