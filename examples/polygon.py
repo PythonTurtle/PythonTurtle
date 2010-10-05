0c2edb04148906e03ef3352cb5b5824cd9b8259b
Spacerat <spacerat3004@gmail.com>
2010-10-05 23:00:11 +0100

    360 should be a float.

--- examples/polygon.py
+++ examples/polygon.py
@@ -1,13 +1,10 @@
 def Polygon(l,n):
     #calculate the polygon's internal angles.
-    ang = 360/n
+    ang = 360.0/n
     
     #draw the polygon
     for i in xrange(n):
         go(l)
         left(ang)
 		
-
-	
-
 Polygon(20,5)
