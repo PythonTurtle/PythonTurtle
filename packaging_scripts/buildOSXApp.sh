c7c01487925e2dba87a26b630365d1b2dc08a35e
Chris van Es <cvanes@gmail.com>
2013-11-30 18:23:05 +0000

    Adding dmg creation to packaging script to make installation easier

--- packaging_scripts/buildOSXApp.sh
+++ packaging_scripts/buildOSXApp.sh
@@ -8,3 +8,13 @@ cd ../../osx_dist
 mv PythonTurtle.app PythonTurtle64.app
 ditto --rsrc --arch i386 PythonTurtle64.app PythonTurtle.app
 rm -rf PythonTurtle64.app
+rm PythonTurtle.dmg
+
+# get the tools we need to create a dmg to make installation easier
+brew update
+brew install npm
+npm install -g appdmg
+
+# create the dmg, uses a very cool tool called appdmg (https://github.com/LinusU/node-appdmg)
+cd ../packaging_scripts
+appdmg plumbing/osx_resources/appdmg.json ../osx_dist/PythonTurtle.dmg
