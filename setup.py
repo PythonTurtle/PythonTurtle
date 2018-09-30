043fe4bb823ff30434bd338d9f20f7ea51e2ee76
Peter Bittner <django@bittner.it>
2018-09-30 13:27:45 +0200

    Add test infrastructure and first tests
    Drop Python 2 from linting and tests

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     license=package.__license__,
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    install_requires=['wxPython'],
+    # install_requires=['wxPython'],
     tests_require=['tox'],
     zip_safe=True,
 )
